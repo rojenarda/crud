9dd5737af2f0c336e5e699073e7b06ad02faaf86
rojenarda <sesen19@itu.edu.tr>
2022-09-08 16:12:14 +0300

    float types are now acceptable as inputs.

--- .aws-sam/build/UpdateUserFunction/app.py
+++ .aws-sam/build/UpdateUserFunction/app.py
@@ -1,12 +1,13 @@
 import boto3
 import json
 import traceback
+from decimal import Decimal
 
 def lambda_handler(event, context):
     try:
         dynamodb = boto3.resource("dynamodb")
         table = dynamodb.Table("user_id_table")
-        data = json.loads(event["body"])
+        data = json.loads(event["body"], parse_float=Decimal)
 
         print("Attempting to add the data to the table.")
         response = table.put_item(Item=data)
